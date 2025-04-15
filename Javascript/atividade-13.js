00e8d652a7a63221e681b68e4a92e6f45b255eab
rafaelcoellhoo <rafaelcoellhoo@gmail.com>
2025-04-15 08:25:44 -0300

    exercicios a mais

---
+++ Javascript/atividade-13.js
@@ -0,0 +1,29 @@
+//crie uma funçao pegue a idade , genero e cidade de um usuario
+//se o usuario for do sexo masculino e tiver mais de 18 anos 
+//exibir precisa se alistar 
+//se o usuario for do sexo feminino e tiver mais de 18 anos
+//exibir pode se alistar 
+//se o usuario for do sexo masculino e tiver menos de 18 anos
+//exibir não pode se alistar 
+
+const user = {
+    nome:"Rafael",
+    idade: 27,
+    genero :"masculino",
+    cidade:"Caucaia"
+
+}
+
+function verificarAlistamento(user){
+    if (user.idade>=18 && user.genero == "masculino"){
+        
+        console.log ("PODE SE ALISTAR")
+    }else{
+
+        console.log ("NAO PODE SE ALISTAR")
+    }
+        
+    }
+   
+   
+ verificarAlistamento(user)
